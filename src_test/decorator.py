c365bc446c53d26aa00b545abe618f2a03b19efb
zhangsh <zhangshnju@gmail.com>
2020-09-08 11:36:17 +0800

    一些新的想法

--- src_test/decorator.py
+++ src_test/decorator.py
@@ -15,10 +15,25 @@ def deco(func):
 def foo():
     print "hello world."
 
+def test():
+    print "hello world"
+    return A()
+
+
+class A():
+    def __init__(self):
+        pass
+
+    def close(self):
+        print "A.close"
+
 
 if __name__ == '__main__':
     # foo()
     # foo._f()
-    a = 'Hello world!'
-    b = a[::-1]
-    print b
+    # a = 'Hello world!'
+    # b = a[::-1]
+    # print b
+    from contextlib import closing
+    with closing(test()) as f:
+        print "end like."
